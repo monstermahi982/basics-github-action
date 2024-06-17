5dc2d44eea08e30657cad041dc708e05f67a54a7
Mahesh Gaikwad <66198736+monstermahi982@users.noreply.github.com>
2024-06-17 12:16:54 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,8 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es6: true,
-  },
-  extends: 'eslint:recommended',
-  rules: {
-    'no-unused-vars': 'warn', // Warn about unused variables
-  },
-};
+export default [
+    {
+        rules: {
+            "no-unused-vars": "error",
+            "no-undef": "error"
+        }
+    }
+];
