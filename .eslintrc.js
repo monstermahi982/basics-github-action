e8917307d3b5011b22751a36dbfccad67f4f7269
Mahesh Gaikwad <66198736+monstermahi982@users.noreply.github.com>
2024-06-17 12:14:30 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'no-unused-vars': 'warn', // Warn about unused variables
+  },
+};
